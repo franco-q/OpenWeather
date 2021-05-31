5da5123996f83c37fe18c6b4396622c75abd0c33
Franco <franco.n.quintana@gmail.com>
2021-05-31 14:20:36 -0300

    added redux-persist 💾

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,14 +2,31 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import reducers from './reducers'
 import Sagas from './sagas'
 
+const persistedReducers = persistReducer(
+	{
+		key: 'root',
+		storage: AsyncStorage
+	},
+	reducers
+)
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const store = createStore(persistedReducers, applyMiddleware(sagaMiddleware))
+const persistor = persistStore(store)
 
 sagaMiddleware.run(Sagas)
 
 export default ({ children }) => {
-	return <Provider store={store}>{children}</Provider>
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	)
 }
